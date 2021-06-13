54a7a9dac8d9dd3a9416d3348d7eb4c790055485
Headlighter <72111471+HEADLIGHTER@users.noreply.github.com>
2021-06-13 07:32:21 +0300

    3773

---
+++ monitoring.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+arc=$(uname -a)
+pcpu=$(grep "physical id" /proc/cpuinfo | sort | uniq | wc -l)
+vcpu=$(grep "^processor" /proc/cpuinfo | wc -l)
+fram=$(free -m | awk '$1 == "Mem:" {print $2}')
+uram=$(free -m | awk '$1 == "Mem:" {print $3}')
+pram=$(free | awk '$1 == "Mem:" {printf("%.2f"), $3/$2*100}')
+fdisk=$(df -H /home/ --output=avail | grep "[0-9]")
+udisk=$(df -m /home/ --output=used | grep "[0-9]")
+pdisk=$(df /home/ --output=pcent | grep "[0-9]")
+cpul=$()
+lb=$(who -b | awk '$1 == "system" {print $3 " " $4}')
+lvmu=$()
+ctcp=$()
+ulog=$(users | wc -w)
+ip=$(hostname -I)
+mac=$(ip link show | awk '$1 == "link/ether" {print $2}')
+cmds=$(sudo journalctl _COMM=sudo | grep COMMAND | wc -l)
+echo "Broadcast message"
+echo
+echo "	#Architecture: $arc"
+echo "	#CPU physical: $pcpu"
+echo "	#vCPU: $vcpu"
+echo "	#Memory Usage: $uram/$fram"MB" ($pram%)"
+echo "	#Disk Usage: $udisk/${fdisk//[[:blank:]]/}"b" (${pdisk//[:blank:]]/})"
+echo "	#CPU load: $cpul"
+echo "	#Last boot: $lb"
+echo "	#LVM use: $lvmu"
+echo "	#Connexions TCP: $ctcp ESTABLISHED"
+echo "	#User log: $ulog"
+echo "	#Network: IP $ip ($mac)"
+echo "	Sudo: $cmds cmd"
